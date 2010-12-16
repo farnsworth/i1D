3c0f3ec2c4ddd2e212fbd9c4bb18cb1277bbf934
farnsworth <simone.ziraldo@gmail.com>
2010-12-16 14:42:14 +0100

    main module

---
+++ all.py
@@ -0,0 +1,13 @@
+
+import sys
+sys.path.append('libs/')
+sys.path.append('plot_scripts/')
+datadir = 'data/'
+import wl
+import pure_wl
+import runs
+import exactplots
+import system
+import utilities
+import ising1D
+import present
